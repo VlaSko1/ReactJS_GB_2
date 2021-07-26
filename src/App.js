d57f1d6d510f64c21f3518926abbd324895089d5
VlaSko1 <vizion84@yandex.ru>
2021-07-26 18:41:28 +0300

    training lesson_4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Image = ({ children }) => {
+  return <div>{children("imageWrapper")}</div>;
+ };
+ 
+ const App = () => {
+  const [imageSrc, setImageSrc] = useState("someImgSrc");
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Image>
+      {(className) => (
+        <div className={className}>
+          <img src={imageSrc} />
+        </div>
+      )}
+    </Image>
   );
-}
+ };
+ 
+
+
+
+
+
+
+
+/*const Image = ({ render }) => {
+  return <div>{render("imageWrapper")}</div>;
+};
+
+
+const App = () => {
+  const [imageSrc, setImageSrc] = useState("someImgSrc");
+ 
+  return (
+    <Image
+      render={(className) => (
+        <div className={className}>
+          <img src={imageSrc} />
+        </div>
+      )}
+    />
+  );
+ };
+ */
 
 export default App;
